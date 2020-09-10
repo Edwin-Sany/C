149f2de24f2276c75e43111c08a359de71c49ba3
Edwin Sany <edwinsany7@gmail.com>
2020-09-10 12:22:25 +0530

    checking pirntf function

---
+++ varibles/printf.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+int i;
+float f;
+char c;
+char str[20];
+
+printf("Enter the integer: ");
+scanf("%d", &i);
+printf("Enter the float: ");
+scanf("%f", &f);
+printf("Enter the char:");
+scanf(" %c", &c);
+
+printf("Integer %d\n", i);
+printf("float %f\n", f);
+printf("char %c\n", c);
+
+
+}
+
