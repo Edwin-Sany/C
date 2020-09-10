3d91554f5b083cc5aa485a485f1428522efe3068
Edwin Sany <edwinsany7@gmail.com>
2020-09-10 13:10:33 +0530

    print integer corresponding to the ascII

---
+++ varibles/ascII.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+
+{
+int integer;
+char character; 
+
+printf("Enter a symbol: ");
+scanf("%c", &character);
+
+integer=character;
+
+printf("The integer currespoding to the character you entered is:%d\n", integer);
+
+}
