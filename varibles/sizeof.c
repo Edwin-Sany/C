ffac53c81c844b585a3360ab56581973e9b8c360
Edwin Sany <edwinsany7@gmail.com>
2020-09-10 11:02:24 +0530

    sizeof function

---
+++ varibles/sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+
+{
+ printf("the size of int is %ld\n", sizeof(int));
+ printf("Size of float is %ld\n", sizeof(float));
+ printf("Size of char is %ld\n", sizeof(char));
+
+}
